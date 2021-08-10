04421046be1ac3518602a4da3865a9e1d62f00cf
Steve Willowby <justus.i.hibshman@gmail.com>
2021-08-10 15:53:07 -0400

    Allowed stars to be computed for all types of graphs - not just flattened ones.

--- look_for_stars.py
+++ look_for_stars.py
@@ -1,75 +1,106 @@
 from graph_sequencer_utility import GraphSequence
 
-def find_stars(temporal_directed_edges):
+def find_stars(edges, directed=True, temporal=True):
 
-    in_neighbors_by_timestamp = {}
-    out_neighbors_by_timestamp = {}
-    for (a, b, t) in temporal_directed_edges:
-        if t not in in_neighbors_by_timestamp:
-            in_neighbors_by_timestamp[t] = {}
-            out_neighbors_by_timestamp[t] = {}
-        i_n_t = in_neighbors_by_timestamp[t]
-        o_n_t = out_neighbors_by_timestamp[t]
-        if a not in o_n_t:
-            o_n_t[a] = set()
-            i_n_t[a] = set()
-        if b not in o_n_t:
-            o_n_t[b] = set()
-            i_n_t[b] = set()
-        o_n_t[a].add(b)
-        i_n_t[b].add(a)
+    if not directed:
+        if temporal:
+            edges = set([(min(a, b), max(a, b), t) for (a, b, t) in edges])
+        else:
+            edges = set([(min(a, b), max(a, b)) for (a, b) in edges])
+
+    edge_colors = {}
+    if not temporal:
+        edge_colors = {(a, b): 0 for (a, b) in edges}
+    else:
+        timestamps_per_edge = {}
+        for (a, b, t) in edges:
+            if (a, b) not in timestamps_per_edge:
+                timestamps_per_edge[(a, b)] = set()
+            timestamps_per_edge[(a, b)].add(t)
+
+        timestamps_to_color = {}
+        next_color = 0
+        for edge, times in timestamps_per_edge.items():
+            times = tuple(sorted(list(times)))
+            if times not in timestamps_to_color:
+                timestamps_to_color[times] = next_color
+                next_color += 1
+            edge_colors[edge] = timestamps_to_color[times]
+
+    # edge_colors just contains the directed information
+    #
+    # connection_types factors in back-edges for colors
+    connection_types = {}
+    if not directed:
+        connection_types = edge_colors
+    elif not temporal:
+        for (a, b), _ in edge_colors.items():
+            if (b, a) in edge_colors:
+                # Bidirected
+                connection_types[(a, b)] = 2
+                connection_types[(b, a)] = 2
+            else:
+                # Out
+                connection_types[(a, b)] = 0
+                # In
+                connection_types[(b, a)] = 1
+    else:
+        # Directed and temporal
+        color_pairs_to_connection_type = {}
+        next_connection_type = 0
+        for (a, b), c in edge_colors.items():
+            if (b, a) in edge_colors:
+                c_alt = edge_colors[(b, a)]
+            else:
+                c_alt = -1
+            if (c, c_alt) not in color_pairs_to_connection_type:
+                color_pairs_to_connection_type[(c,c_alt)] = next_connection_type
+                next_connection_type += 1
+            if (c_alt, c) not in color_pairs_to_connection_type:
+                color_pairs_to_connection_type[(c_alt,c)] = next_connection_type
+                next_connection_type += 1
+            connection_types[(a, b)] = color_pairs_to_connection_type[(c,c_alt)]
+            connection_types[(b, a)] = color_pairs_to_connection_type[(c_alt,c)]
+
+    # At this point, connection_types is "bidirected"
+    for (a, b), _ in connection_types.items():
+        assert (b, a) in connection_types
+
+    neighbors_by_type = {}
+    num_neighbors = {}
+    for (a, b), type_value in connection_types.items():
+        if a not in num_neighbors:
+            num_neighbors[a] = 0
+            neighbors_by_type[a] = {}
+        if type_value not in neighbors_by_type[a]:
+            neighbors_by_type[a][type_value] = set()
+
+        num_neighbors[a] += 1
+        neighbors_by_type[a][type_value].add(b)
 
     stars = []
     partial_stars = []
-    bidir_pairs = []
-    for t, i_n_t in in_neighbors_by_timestamp.items():
-        o_n_t = out_neighbors_by_timestamp[t]
-
-        # Inward star (e.g. followers)
-        for node, in_neighbors in i_n_t.items():
-            if len(in_neighbors) == 0:
-                continue
-            num_single_neighbors = 0
-            for neighbor in in_neighbors:
-                if len(o_n_t[neighbor]) + len(i_n_t[neighbor]) == 1:
-                    num_single_neighbors += 1
-            if num_single_neighbors == len(in_neighbors):
-                stars.append((num_single_neighbors, node))
-            elif num_single_neighbors >= 2:
-                partial_stars.append((num_single_neighbors, node))
-
-        # Outward star (e.g. text recipients)
-        for node, out_neighbors in o_n_t.items():
-            if len(out_neighbors) == 0:
-                continue
-            num_single_neighbors = 0
-            for neighbor in out_neighbors:
-                if len(o_n_t[neighbor]) + len(i_n_t[neighbor]) == 1:
-                    num_single_neighbors += 1
-            if num_single_neighbors == len(out_neighbors):
-                stars.append((num_single_neighbors, node))
-            elif num_single_neighbors >= 2:
-                partial_stars.append((num_single_neighbors, node))
-
-        # Bidirectional star
-        for node, out_neighbors in o_n_t.items():
-            if len(out_neighbors & i_n_t[node]) == 0:
-                continue
-            num_single_neighbors = 0
-            for neighbor in out_neighbors & i_n_t[node]:
-                if len(o_n_t[neighbor]) + len(i_n_t[neighbor]) == 2:
-                    num_single_neighbors += 1
-            if num_single_neighbors == len(out_neighbors & i_n_t[node]):
-                if num_single_neighbors == 1 and len(out_neighbors) == 1:
-                    bidir_pairs.append((2, node))
+    pairs = []
+    for node, n_b_t in neighbors_by_type.items():
+        for type_value, neighbors in n_b_t.items():
+            num_singleton_neighbors = 0
+            for neighbor in neighbors:
+                if num_neighbors[neighbor] == 1:
+                    num_singleton_neighbors += 1
+            if num_singleton_neighbors == num_neighbors[node]:
+                if num_singleton_neighbors == 1:
+                    if connection_types[(node, neighbor)] == \
+                            connection_types[(neighbor, node)] and \
+                                node < neighbor:
+                        pairs.append((node, neighbor))
                 else:
-                    stars.append((num_single_neighbors, node))
-            elif num_single_neighbors >= 2:
-                partial_stars.append((num_single_neighbors, node))
+                    stars.append((num_singleton_neighbors, node))
+            elif num_singleton_neighbors > 1:
+                partial_stars.append((num_singleton_neighbors, node))
 
     stars.sort(reverse=True)
     partial_stars.sort(reverse=True)
-    return (stars, partial_stars, bidir_pairs)
+    return (stars, partial_stars, pairs)
 
 if __name__ == "__main__":
     # Day, Week resolution
@@ -85,7 +116,7 @@ if __name__ == "__main__":
         units_per_window=7, \
         start_offset_number=-28561, \
         windows_overlap=False, \
-        flatten_window=True, \
+        flatten_window=False, \
         weight_repeats=False, \
         directed=True)
 
@@ -96,7 +127,8 @@ if __name__ == "__main__":
     while window_GS.has_next():
         print("Week %d" % i)
         (nodes, edges) = window_GS.next()
-        (stars, partial_stars, _) = find_stars(temporal_directed_edges=edges)
+        (stars, partial_stars, _) = \
+            find_stars(edges, temporal=True, directed=True)
         if len(stars) > 0:
             print("  Largest Star: %d" % stars[0][0])
             if stars[0][0] > largest_star:
@@ -122,7 +154,7 @@ if __name__ == "__main__":
         units_per_window=24, \
         start_offset_number=-28561, \
         windows_overlap=False, \
-        flatten_window=True, \
+        flatten_window=False, \
         weight_repeats=False, \
         directed=True)
     print("Daily Analysis")
@@ -132,7 +164,8 @@ if __name__ == "__main__":
     while window_GS.has_next():
         print("Day %d" % i)
         (nodes, edges) = window_GS.next()
-        (stars, partial_stars, _) = find_stars(temporal_directed_edges=edges)
+        (stars, partial_stars, _) = \
+            find_stars(edges, temporal=True, directed=True)
         if len(stars) > 0:
             print("  Largest Star: %d" % stars[0][0])
             if stars[0][0] > largest_star:
