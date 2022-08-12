930276b4ee4bbfae341e008751fd1d8a9bbb792b
Steve Willowby <justus.i.hibshman@gmail.com>
2022-08-12 15:41:39 -0400

    Changed so that git just keeps the original version

--- c_nauty_traces_setup.sh
+++ c_nauty_traces_setup.sh
@@ -3,3 +3,6 @@
 cd nauty27r4
 ./configure
 make
+git update-index --assume-unchanged nauty27r4/nauty.h
+git update-index --assume-unchanged nauty27r4/makefile
+git update-index --assume-unchanged nauty27r4/gtools.h
