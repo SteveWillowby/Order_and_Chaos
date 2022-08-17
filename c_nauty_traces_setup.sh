eae27baf7e2dea7f4cdb5a8fc32a3ceebc1ee5c4
Steve Willowby <justus.i.hibshman@gmail.com>
2022-08-17 18:18:17 -0400

    currently not using assume-unchanged

--- c_nauty_traces_setup.sh
+++ c_nauty_traces_setup.sh
@@ -3,6 +3,6 @@
 cd nauty27r4
 ./configure --enable-tls
 make
-git update-index --assume-unchanged nauty27r4/nauty.h
-git update-index --assume-unchanged nauty27r4/makefile
-git update-index --assume-unchanged nauty27r4/gtools.h
+# git update-index --assume-unchanged nauty27r4/nauty.h
+# git update-index --assume-unchanged nauty27r4/makefile
+# git update-index --assume-unchanged nauty27r4/gtools.h
