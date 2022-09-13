56d958d89a8cfc185cae11ee7d6ec451f61890bf
Steve Willowby <justus.i.hibshman@gmail.com>
2022-09-13 19:09:12 -0400

    Moving compiled executables to a folder.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # For some reason gcc has issues on this machine.
-g++ -o nt_graph_test -std=c++11 nt_graph_test.cpp graph.cpp sparse_graph.cpp nauty27r4/nauty.a
+g++ -o executables/nt_graph_test -std=c++11 nt_graph_test.cpp graph.cpp sparse_graph.cpp nauty27r4/nauty.a
