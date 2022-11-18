206a2c0e20118e0675e373967bd07e791abaedf7
Steve Willowby <__na__>
2022-11-18 14:10:48 -0500

    ...

--- nt_graph_test.cpp
+++ nt_graph_test.cpp
@@ -43,17 +43,27 @@ int main(void) {
              <<"###################################################"<<std::endl
              <<std::endl;
 
-    NTSparseGraph g_undir = NTSparseGraph(false, 7);
-    g_undir.add_edge(0, 1);
-    g_undir.add_edge(2, 1);
-    // 4 * 3 * 2 = 24
-
     SYMTracesOptions options;
     options.get_node_orbits = true;
     options.get_edge_orbits = true;
     options.get_canonical_node_order = true;
 
+    NTSparseGraph g_undir = NTSparseGraph(false, 7);
+
     SYMTracesResults result = traces(g_undir, options);
+    std::cout<<std::endl;
+    std::cout<<"// Undirected Graph on 7 nodes with no edges"<<std::endl;
+    std::cout<<"|Aut(G)| = "<<result.num_aut_base<<" x 10^"<<result.num_aut_exponent<<std::endl;
+    std::cout<<"Num Orbits: "<<result.num_node_orbits<<std::endl;
+    std::cout<<"Error Status: "<<result.error_status<<std::endl;
+    std::cout<<"Node Orbits: "<<vec_as_string(result.node_orbits)<<std::endl;
+    std::cout<<"Canonical Order: "<<vec_as_string(result.canonical_node_order)<<std::endl;
+
+    g_undir.add_edge(0, 1);
+    g_undir.add_edge(2, 1);
+
+    result = traces(g_undir, options);
+    std::cout<<std::endl;
     std::cout<<"// Undirected Graph on 7 nodes with edges (0, 1), (1, 2)"<<std::endl;
     std::cout<<"|Aut(G)| = "<<result.num_aut_base<<" x 10^"<<result.num_aut_exponent<<std::endl;
     std::cout<<"Num Orbits: "<<result.num_node_orbits<<std::endl;
@@ -77,7 +87,7 @@ int main(void) {
     g_dir.add_edge(1, 2);
     result = traces(g_dir, options);
     std::cout<<std::endl;
-    std::cout<<"// Directed Graph on 7 nodes with edge (0, 1), (1, 2)"<<std::endl;
+    std::cout<<"// Directed Graph on 7 nodes with edges (0, 1), (1, 2)"<<std::endl;
     std::cout<<"|Aut(G)| = "<<result.num_aut_base<<" x 10^"<<result.num_aut_exponent<<std::endl;
     std::cout<<"Num Orbits: "<<result.num_node_orbits<<std::endl;
     std::cout<<"Error Status: "<<result.error_status<<std::endl;
