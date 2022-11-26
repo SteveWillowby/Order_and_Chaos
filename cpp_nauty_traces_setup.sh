b82b02c4a2dce76515e605223991884b850a321d
Steve Willowby <__na__>
2022-11-25 20:44:55 -0500

    ...

--- cpp_nauty_traces_setup.sh
+++ cpp_nauty_traces_setup.sh
@@ -3,5 +3,5 @@
 cd nauty27r4_modified
 # Compile for C++ use. This changes the type used for thread-local storage
 #   from _Thread_Local to thread_local .
-./configure CC=g++ CFLAGS="-std=c++11" --enable-tls
+./configure CC=g++ CFLAGS="-std=c++11 -O4" --enable-tls
 make
