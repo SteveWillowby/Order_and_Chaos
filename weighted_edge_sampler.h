133c2d08a65ffed7ea9baafc0a959b16b318bf43
Steve Willowby <__na__>
2023-03-15 15:09:56 -0400

    new file

---
+++ weighted_edge_sampler.h
@@ -0,0 +1,14 @@
+#include "edge_sampler.h"
+
+#ifndef SYM__WEIGHTED_EDGE_SAMPLER_H
+#define SYM__WEIGHTED_EDGE_SAMPLER_H
+
+class WeightedEdgeSampler : public EdgeSampler {
+public:
+
+
+protected:
+
+};
+
+#endif
