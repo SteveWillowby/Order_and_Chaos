0722802bb84f06d2775e0f6d18fc1aeb70e97ec0
Steve Willowby <__na__>
2023-07-06 12:02:56 -0400

    Added the necessary flag

--- experiments/structure_recovery/no_samp_h_helper.sh
+++ experiments/structure_recovery/no_samp_h_helper.sh
@@ -17,6 +17,7 @@ echo "Johnson 0.002 t1"
 time nice -2 ../../executables/main -graph johnson_10_3_120_edges.txt \
                                     -nodes johnson_10_3_120_nodes.txt \
                                     -noise+ 0.002 -noise- 0.002 -o results/nsh_johnson_002_1 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_johnson_002_1.txt
 
 echo "Johnson 0.002 t2"
@@ -24,6 +25,7 @@ echo "Johnson 0.002 t2"
 time nice -2 ../../executables/main -graph johnson_10_3_120_edges.txt \
                                     -nodes johnson_10_3_120_nodes.txt \
                                     -noise+ 0.002 -noise- 0.002 -o results/nsh_johnson_002_2 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_johnson_002_2.txt
 
 echo "Johnson 0.002 t3"
@@ -31,6 +33,7 @@ echo "Johnson 0.002 t3"
 time nice -2 ../../executables/main -graph johnson_10_3_120_edges.txt \
                                     -nodes johnson_10_3_120_nodes.txt \
                                     -noise+ 0.002 -noise- 0.002 -o results/nsh_johnson_002_3 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_johnson_002_3.txt
 
 echo "Johnson 0.003 t1"
@@ -39,6 +42,7 @@ time nice -2 ../../executables/main -graph johnson_10_3_120_edges.txt \
                                     -nodes johnson_10_3_120_nodes.txt \
                                     -noise+ 0.003 -noise- 0.003 -o results/nsh_johnson_003_1 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_johnson_003_1.txt
 
 echo "Johnson 0.003 t2"
@@ -47,6 +51,7 @@ time nice -2 ../../executables/main -graph johnson_10_3_120_edges.txt \
                                     -nodes johnson_10_3_120_nodes.txt \
                                     -noise+ 0.003 -noise- 0.003 -o results/nsh_johnson_003_2 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_johnson_003_2.txt
 
 echo "Johnson 0.003 t3"
@@ -55,6 +60,7 @@ time nice -2 ../../executables/main -graph johnson_10_3_120_edges.txt \
                                     -nodes johnson_10_3_120_nodes.txt \
                                     -noise+ 0.003 -noise- 0.003 -o results/nsh_johnson_003_3 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_johnson_003_3.txt
 
 # Bin Tree
@@ -66,6 +72,7 @@ echo "Bin Tree 0.01"
 time nice -2 ../../executables/main -graph binary_tree_127_edges.txt \
                                     -nodes binary_tree_127_nodes.txt \
                                     -noise+ 0.01 -noise- 0.01 -o results/nsh_bin_tree_01 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_bin_tree_01.txt
 
 echo "Bin Tree 0.02"
@@ -73,6 +80,7 @@ echo "Bin Tree 0.02"
 time nice -2 ../../executables/main -graph binary_tree_127_edges.txt \
                                     -nodes binary_tree_127_nodes.txt \
                                     -noise+ 0.02 -noise- 0.02 -o results/nsh_bin_tree_02 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_bin_tree_02.txt
 
 echo "Bin Tree 0.03"
@@ -80,6 +88,7 @@ echo "Bin Tree 0.03"
 time nice -2 ../../executables/main -graph binary_tree_127_edges.txt \
                                     -nodes binary_tree_127_nodes.txt \
                                     -noise+ 0.03 -noise- 0.03 -o results/nsh_bin_tree_03 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_bin_tree_03.txt
 
 echo "Bin Tree 0.04"
@@ -88,6 +97,7 @@ time nice -2 ../../executables/main -graph binary_tree_127_edges.txt \
                                     -nodes binary_tree_127_nodes.txt \
                                     -noise+ 0.04 -noise- 0.04 -o results/nsh_bin_tree_04 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_bin_tree_04.txt
 
 echo "Bin Tree 0.05"
@@ -96,6 +106,7 @@ time nice -2 ../../executables/main -graph binary_tree_127_edges.txt \
                                     -nodes binary_tree_127_nodes.txt \
                                     -noise+ 0.05 -noise- 0.05 -o results/nsh_bin_tree_05 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_bin_tree_05.txt
 
 # Ring
@@ -107,6 +118,7 @@ echo "Ring 0.01"
 time nice -2 ../../executables/main -graph ring_128_edges.txt \
                                     -nodes ring_128_nodes.txt \
                                     -noise+ 0.01 -noise- 0.01 -o results/nsh_ring_01 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_ring_01.txt
 
 echo "Ring 0.02"
@@ -114,6 +126,7 @@ echo "Ring 0.02"
 time nice -2 ../../executables/main -graph ring_128_edges.txt \
                                     -nodes ring_128_nodes.txt \
                                     -noise+ 0.02 -noise- 0.02 -o results/nsh_ring_02 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_ring_02.txt
 
 echo "Ring 0.03"
@@ -121,6 +134,7 @@ echo "Ring 0.03"
 time nice -2 ../../executables/main -graph ring_128_edges.txt \
                                     -nodes ring_128_nodes.txt \
                                     -noise+ 0.03 -noise- 0.03 -o results/nsh_ring_03 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_ring_03.txt
 
 echo "Ring 0.04"
@@ -129,6 +143,7 @@ time nice -2 ../../executables/main -graph ring_128_edges.txt \
                                     -nodes ring_128_nodes.txt \
                                     -noise+ 0.04 -noise- 0.04 -o results/nsh_ring_04 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_ring_04.txt
 
 echo "Ring 0.05"
@@ -137,6 +152,7 @@ time nice -2 ../../executables/main -graph ring_128_edges.txt \
                                     -nodes ring_128_nodes.txt \
                                     -noise+ 0.05 -noise- 0.05 -o results/nsh_ring_05 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_ring_05.txt
 
 # Wreath
@@ -149,6 +165,7 @@ time nice -2 ../../executables/main -graph wreath_d7_128_edges.txt \
                                     -nodes wreath_d7_128_nodes.txt \
                                     -noise+ 0.0025 -noise- 0.0025 -o results/nsh_wreath_0025 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_wreath_0025.txt
 
 echo "Wreath 0.005"
@@ -157,6 +174,7 @@ time nice -2 ../../executables/main -graph wreath_d7_128_edges.txt \
                                     -nodes wreath_d7_128_nodes.txt \
                                     -noise+ 0.005 -noise- 0.005 -o results/nsh_wreath_005 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_wreath_005.txt
 
 echo "Wreath 0.01"
@@ -165,6 +183,7 @@ time nice -2 ../../executables/main -graph wreath_d7_128_edges.txt \
                                     -nodes wreath_d7_128_nodes.txt \
                                     -noise+ 0.01 -noise- 0.01 -o results/nsh_wreath_01 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_wreath_01.txt
 
 echo "Wreath 0.02"
@@ -173,6 +192,7 @@ time nice -2 ../../executables/main -graph wreath_d7_128_edges.txt \
                                     -nodes wreath_d7_128_nodes.txt \
                                     -noise+ 0.02 -noise- 0.02 -o results/nsh_wreath_02 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_wreath_02.txt
 
 echo "Wreath 0.04"
@@ -181,4 +201,5 @@ time nice -2 ../../executables/main -graph wreath_d7_128_edges.txt \
                                     -nodes wreath_d7_128_nodes.txt \
                                     -noise+ 0.04 -noise- 0.04 -o results/nsh_wreath_04 \
                                     -n_itr 120 \
+                                    -no_sample_heuristic \
                                     >> results/nsh_wreath_04.txt
