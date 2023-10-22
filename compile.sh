5e2bf57bd0c920efefb25e6062d85958445a41ca
Steve Willowby <__na__>
2023-10-22 16:21:54 -0400

    file to get score of a single noise set

--- compile.sh
+++ compile.sh
@@ -8,4 +8,6 @@ g++ -Wall -Wextra -o executables/graph_enumeration -std=c++11 -O4 graph_enumerat
 
 g++ -Wall -Wextra -o executables/mthread_scorer -std=c++11 -O4 test_threaded_scorer.cpp nt_partition.cpp graph.cpp sparse_graph.cpp nt_sparse_graph.cpp nauty_traces.cpp file_utils.cpp edge_sampler.cpp scoring_function.cpp wl_measures.cpp Jonker_Volgenant/src/assign2DCBasic.c int_edge_sampler.cpp thread_pool_wl_sim.cpp thread_pool_scorer.cpp nauty27r4_modified/nauty.a -lpthread
 
+g++ -Wall -Wextra -o executables/score_only -std=c++11 -O4 score_only.cpp nt_partition.cpp graph.cpp sparse_graph.cpp nt_sparse_graph.cpp nauty_traces.cpp file_utils.cpp edge_sampler.cpp scoring_function.cpp wl_measures.cpp Jonker_Volgenant/src/assign2DCBasic.c int_edge_sampler.cpp thread_pool_wl_sim.cpp thread_pool_scorer.cpp noise_prob_choice.cpp nauty27r4_modified/nauty.a -lpthread
+
 g++ -Wall -Wextra -o executables/main -std=c++11 -O4 main.cpp nt_partition.cpp graph.cpp sparse_graph.cpp nt_sparse_graph.cpp nauty_traces.cpp file_utils.cpp edge_sampler.cpp scoring_function.cpp wl_measures.cpp Jonker_Volgenant/src/assign2DCBasic.c int_edge_sampler.cpp thread_pool_scorer.cpp thread_pool_wl_sim.cpp noise_prob_choice.cpp genetic_alg_search.cpp simulated_annealing_search.cpp nauty27r4_modified/nauty.a -lpthread
---
+++ score_only.cpp
@@ -0,0 +1,307 @@
+#include<algorithm>
+#include<cmath>
+#include<iostream>
+#include<random>
+#include<unordered_set>
+#include<stdexcept>
+#include<string>
+#include<vector>
+
+#include "edge.h"
+#include "edge_sampler.h"
+#include "file_utils.h"
+#include "genetic_alg_search.h"
+#include "nauty_traces.h"
+#include "noise_prob_choice.h"
+#include "nt_sparse_graph.h"
+#include "simulated_annealing_search.h"
+#include "sparse_graph.h"
+
+std::string get_cmd_option(const std::vector<std::string>& inputs,
+                           const std::string& option) {
+    for (size_t i = 0; i < inputs.size(); i++) {
+        if (inputs[i] == option && i < inputs.size() - 1) {
+            return inputs[i + 1];
+        }
+    }
+    throw std::invalid_argument("Error! Expected argument for flag " + option);
+}
+
+bool cmd_flag_present(const std::vector<std::string>& inputs,
+                      const std::string& option) {
+    for (size_t i = 0; i < inputs.size(); i++) {
+        if (inputs[i] == option) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(int argc, char* argv[]) {
+    if (2 * sizeof(int) > sizeof(long)) {
+        std::cout<<sizeof(int)<<std::endl;
+        std::cout<<"<<< Warning! 2 * sizeof(int) > sizeof(long)"
+                 <<std::endl<<"\tThis may cause problems if you use a node id "
+                 <<">= "<<(long(1) << ((sizeof(int) * 8) / 2))<<" >>>"
+                 <<std::endl;
+    }
+
+    std::vector<std::string> inputs = std::vector<std::string>();
+    for (int i = 1; i < argc; i++) {
+        inputs.push_back(argv[i]);
+    }
+
+    if (cmd_flag_present(inputs, "-h")) {
+        std::cout<<"Guide to flags:"<<std::endl
+                 <<"REQUIRED:"<<std::endl<<std::endl
+                 <<"-graph <arg>:\tedgelist filename (including path)"
+                 <<std::endl<<"\t\t\t* If a nodelist is also specified, "
+                 <<"then all nodes"<<std::endl<<"\t\t\t\tin the nodelist will "
+                 <<"be included."<<std::endl<<"\t\t\t* If a nodelist is NOT "
+                 <<"specified, then the "<<std::endl<<"\t\t\t\tgraph's nodes "
+                 <<"will be 0, 1, 2, ..., X, where "<<std::endl
+                 <<"\t\t\t\tX is the max node label in the edgelist."
+                 <<std::endl<<std::endl
+                 <<"-edges <arg>:\tedgelist filename (including path)"
+                 <<std::endl<<std::endl
+                 <<std::endl<<"OPTIONAL:"<<std::endl<<std::endl
+                 <<"-nodes <arg>:\tnodelist filename (including path)"
+                 <<std::endl<<std::endl
+                 <<"-d and -u:\tdirected and undirected respectively"
+                 <<std::endl<<"\t\t\t* defaults to undirected"
+                 <<std::endl<<std::endl
+                 <<"-p+ <arg>:\tUsed in the scoring function as prob. edges"
+                 <<" were added"<<std::endl<<"\t\t\t* default calculated based"
+                 <<" on input graph"
+                 <<std::endl<<"\t\t\t* if used, must also use p-"
+                 <<std::endl<<std::endl
+                 <<"-p- <arg>:\tUsed in the scoring function as prob. edges"
+                 <<" were removed"<<std::endl<<"\t\t\t* default calculated"
+                 <<" based on input graph"
+                 <<std::endl<<"\t\t\t* if used, must also use p+"
+                 <<std::endl<<std::endl
+                 ;
+
+        return 0;
+    }
+
+    bool directed;
+    if (cmd_flag_present(inputs, "-d")) {
+        if (cmd_flag_present(inputs, "-u")) {
+            throw std::invalid_argument("Error! Cannot pass both -u and -d");
+        }
+        directed = true;
+    } else if (cmd_flag_present(inputs, "-u")) {
+        directed = false;
+    } else {
+        // std::cout<<"No (un)directed flag passed. Assuming undirected."
+        //          <<std::endl;
+        directed = false;
+    }
+
+    std::string graph_file;
+    graph_file = get_cmd_option(inputs, "-graph");
+
+    std::string edgelist_file;
+    edgelist_file = get_cmd_option(inputs, "-edges");
+
+//    std::string output_file;
+//    output_file = get_cmd_option(inputs, "-o");
+
+    std::string nodelist_file = "";  // No nodelist
+    if (cmd_flag_present(inputs, "-nodes")) {
+        nodelist_file = get_cmd_option(inputs, "-nodes");
+    }
+
+    bool custom_p_values = false;
+
+    long double p_plus = 0.0;
+    if (cmd_flag_present(inputs, "-p+")) {
+        custom_p_values = true;
+        p_plus = std::stold(get_cmd_option(inputs, "-p+"));
+        if (p_plus <= 0.0) {
+            throw std::invalid_argument("Error! p+ must be > 0");
+        }
+    }
+
+    long double p_minus = 0.0;
+    if (cmd_flag_present(inputs, "-p-")) {
+        custom_p_values = true;
+        p_minus = std::stold(get_cmd_option(inputs, "-p-"));
+        if (p_minus <= 0.0) {
+            throw std::invalid_argument("Error! p- must be > 0");
+        }
+    }
+
+    if (custom_p_values && (p_plus == 0.0 || p_minus == 0.0)) {
+        throw std::invalid_argument(
+                "Error! Cannot provide p+ without p- and vice versa");
+    }
+
+    // const bool corrupt_original = false;
+    // Only used when corrupt_original is true.
+    // const size_t num_additions = 0;
+    // Only used when corrupt_original is true.
+    // const size_t num_removals = 0;
+
+    NautyTracesOptions o;
+    o.get_node_orbits = true;
+    o.get_edge_orbits = true;
+    o.get_canonical_node_order = false;
+
+    NautyTracesResults nt_results;
+
+    std::cout<<"Loading graph from files:"<<std::endl
+             <<"    "<<nodelist_file<<std::endl
+             <<"    "<<graph_file<<std::endl;
+    SparseGraph g(directed);
+    if (nodelist_file.empty()) {
+        g = read_graph(directed, graph_file);
+    } else {
+        g = read_graph(directed, nodelist_file, graph_file);
+    }
+    std::cout<<"  ...graph loaded. It has "<<g.num_nodes()<<" nodes and "
+             <<g.num_edges()<<" edges, "<<g.num_loops()<<" of which are "
+             <<"self-loops."<<std::endl<<std::endl;
+
+    if (g.num_loops() > 0) {
+        std::cout<<"<<< The current code cannot handle self-loops. "
+                 <<"Ignoring all. >>>"<<std::endl<<std::endl;
+        for (size_t i = 0; i < g.num_nodes(); i++) {
+            if (g.has_edge(i, i)) {
+                g.delete_edge(i, i);
+            }
+        }
+    }
+
+    SparseGraph noise_candidate(directed, g.num_nodes());
+    if (nodelist_file.empty()) {
+        noise_candidate = read_graph(directed, edgelist_file);
+    } else {
+        noise_candidate = read_graph(directed, nodelist_file,
+                                     edgelist_file);
+    }
+
+    bool has_self_loops = g.num_loops() > 0;
+
+    NTSparseGraph g_info = NTSparseGraph(g);
+    nt_results = traces(g_info, o);
+    double log2_aut = std::log2l(nt_results.num_aut_base) +
+                     ((long double)(nt_results.num_aut_exponent)) *
+                                      std::log2l(10);
+    std::cout<<"The original graph has log2_aut = "<<log2_aut<<std::endl;
+
+    std::unordered_set<Edge, EdgeHash> deletions, additions;
+    std::vector<Edge> deletions_sorted, additions_sorted;
+    std::vector<Edge> flip_vec;
+
+    size_t max_possible_edges =
+            (g.num_nodes() * (g.num_nodes() - 1)) / (1 + size_t(!directed)) +
+            (g.num_nodes() * size_t(g.num_loops() > 0));
+
+    size_t max_edit_factor = 7;
+    size_t max_flip_or_edge = (g.num_edges() < (max_possible_edges / 2) ?
+                          g.num_edges() : (max_possible_edges - g.num_edges()));
+    max_flip_or_edge *= max_edit_factor;
+
+    if (max_flip_or_edge < g.num_nodes() * 2) {
+        max_flip_or_edge = g.num_nodes() * 2;
+    }
+    CombinatoricUtility comb_util(max_possible_edges, max_flip_or_edge);
+
+    deletions = std::unordered_set<Edge, EdgeHash>();
+    additions = std::unordered_set<Edge, EdgeHash>();
+
+    // Flip edges randomly
+    for (size_t i = 0; i < g.num_nodes(); i++) {
+        for (size_t j = i * (!directed); j < g.num_nodes(); j++) {
+            if (i == j and !has_self_loops) {
+                continue;
+            }
+            if (g.has_edge(i, j) && noise_candidate.has_edge(i, j)) {
+                deletions.insert(EDGE(i, j, directed));
+            }
+            else if (noise_candidate.has_edge(i, j)) {
+                additions.insert(EDGE(i, j, directed));
+            }
+        }
+    }
+
+    /*
+    additions_sorted= std::vector<Edge>(additions.begin(),
+                                        additions.end());
+    deletions_sorted= std::vector<Edge>(deletions.begin(),
+                                        deletions.end());
+    std::sort(additions_sorted.begin(),
+              additions_sorted.end());
+    std::sort(deletions_sorted.begin(),
+              deletions_sorted.end());
+
+    std::cout<<std::endl<<"Adding: ";
+    for (auto e = additions_sorted.begin();
+              e != additions_sorted.end(); e++) {
+        std::cout<<"("<<e->first<<", "<<e->second<<"), ";
+        g.add_edge(e->first, e->second);
+    }
+    std::cout<<std::endl;
+    std::cout<<std::endl<<"Removing: ";
+    for (auto e = deletions_sorted.begin();
+              e != deletions_sorted.end(); e++) {
+        std::cout<<"("<<e->first<<", "<<e->second<<"), ";
+        g.delete_edge(e->first, e->second);
+    }
+    std::cout<<std::endl;
+    */
+
+    NTSparseGraph g_nt = NTSparseGraph(g);
+    std::vector<long double> log_probs;
+    if (custom_p_values) {
+        log_probs = {std::log2l(p_plus),
+                     std::log2l(1.0 - p_plus),
+                     std::log2l(p_minus),
+                     std::log2l(1.0 - p_minus)};
+    } else {
+        // log_probs = {-1.0, -1.0, -1.0, -1.0};
+        log_probs = default_log2_noise_probs(g_nt, comb_util);
+    }
+
+//    se_del = deletions;
+//    se_add = additions;
+
+    Coloring<Edge, EdgeHash> edge_coloring;
+    for (auto x =  nt_results.edge_orbits.colors().begin();
+              x != nt_results.edge_orbits.colors().end(); x++) {
+        for (auto y =  nt_results.edge_orbits.cell(*x).begin();
+                  y != nt_results.edge_orbits.cell(*x).end(); y++) {
+            edge_coloring.set(*y, *x);
+        }
+    }
+
+    long double s = score(g_nt, comb_util,
+                          nt_results.node_orbits,
+                          nt_results.edge_orbits,
+                          edge_coloring,
+                          additions, deletions,
+                          log_probs[0], log_probs[2],
+                          log_probs[1], log_probs[3],
+                          max_flip_or_edge);
+
+    for (auto x = additions.begin(); x != additions.end(); x++) {
+        g_nt.flip_edge(x->first, x->second);
+    }
+    for (auto x = deletions.begin(); x != deletions.end(); x++) {
+        g_nt.flip_edge(x->first, x->second);
+    }
+
+    nt_results = traces(g_nt, o);
+    log2_aut   = std::log2l(nt_results.num_aut_base) +
+                     ((long double)(nt_results.num_aut_exponent)) *
+                                      std::log2l(10);
+
+    std::cout<<std::endl;
+    std::cout<<"The modified graph has log2_aut = "<<log2_aut<<std::endl;
+
+    std::cout<<std::endl<<"The score is:"<<std::endl<<s<<std::endl;
+
+    return 0;
+}
