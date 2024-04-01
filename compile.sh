4538a10666d27395778489e0547ab73669f3bd0f
Steve Willowby <__na__>
2024-04-01 16:57:08 -0400

    ...

--- compile.sh
+++ compile.sh
@@ -4,6 +4,8 @@
 #   Removed -Werror due to a warning within the Nauty/Traces code itself
 g++ -Wall -Wextra -o test_nt_code -std=c++11 -O4 test_nt_code.cpp nt_wrappers/nt_wrappers.a
 
+g++ -Wall -Wextra -o minimal_nt_example -std=c++11 minimal_nt_example.cpp nt_wrappers/nt_wrappers.a
+
 g++ -Wall -Wextra -o executables/graph_enumeration -std=c++11 -O4 graph_enumeration.cpp nt_wrappers/nt_wrappers.a
 
 g++ -Wall -Wextra -o executables/mthread_scorer -std=c++11 -O4 test_threaded_scorer.cpp nt_wrappers/nt_wrappers.a edge_sampler.cpp scoring_function.cpp wl_measures.cpp Jonker_Volgenant/src/assign2DCBasic.c int_edge_sampler.cpp thread_pool_wl_sim.cpp thread_pool_scorer.cpp -lpthread
--- compile_nt_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# If using gcc instead of g++, add a link to -lstdc++
-#   Removed -Werror due to a warning within the Nauty/Traces code itself
-g++ -Wall -Wextra -o test_nt_code -std=c++11 -O4 test_nt_code.cpp nt_wrappers/nt_wrappers.a
-
-g++ -Wall -Wextra -o minimal_nt_example -std=c++11 minimal_nt_example.cpp nt_wrappers/nt_wrappers.a
